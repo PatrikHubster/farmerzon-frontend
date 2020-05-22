98b104dc2fabbcbd87ce429fbaea5c812d2a1b63
lukas braun <lukas.braun@mailbox.org>
2020-05-22 17:55:51 +0200

    Change Start Page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,58 @@
+import { Products } from '../components/Product/Products';
+
+const products = [
+  {
+    id: 1,
+    name: 'Milch',
+    description: 'frisch',
+    price: 3.24,
+    size: 2,
+    amount: 100,
+    unit: 'l'
+  },
+  {
+    id: 2,
+    name: 'Milch',
+    description: 'frisch',
+    price: 3.24,
+    size: 2,
+    amount: 100,
+    unit: 'l'
+  },
+  {
+    id: 3,
+    name: 'Milch',
+    description: 'frisch',
+    price: 3.24,
+    size: 2,
+    amount: 100,
+    unit: 'l'
+  },
+  {
+    id: 4,
+    name: 'Milch',
+    description: 'frisch',
+    price: 3.24,
+    size: 2,
+    amount: 100,
+    unit: 'l'
+  },
+  {
+    id: 5,
+    name: 'Milch',
+    description: 'frisch',
+    price: 3.24,
+    size: 2,
+    amount: 100,
+    unit: 'l'
+  },
+];
+
 const Home = () => {
   return (
     <div className="container">
       Main Page
+      <Products articles={products} className="container" />
       <style jsx>{`
         .container {
           min-height: 100vh;
