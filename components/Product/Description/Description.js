d13c40d3c84fbc176ab9e794d55d56b8198efbcb
lukas braun <lukas.braun@mailbox.org>
2020-05-23 12:20:29 +0200

    Refactor Product-Card

--- components/Product/Description/Description.js
+++
@@ -1,9 +0,0 @@
-import styles from './Description.module.css';
-
-const Description = (props) => (
-  <div className="description">
-    <textarea readOnly className={styles.desc} value={props.description} />
-  </div>
-)
-
-export default Description;
--- components/Product/Product.jsx
+++ components/Product/Product.jsx
@@ -1,22 +1,18 @@
 import Amount from './Amount/Amount';
-import Description from './Description/Description';
-import Name from './Name/Name';
 import Price from './Price/Price';
 import Size from './Size/Size';
-import styles from  './Product.module.css';
+import styles from './Product.module.css';
 import Card from '../Card/Card';
 
 export const Product = (props) => (
-  <Card>
-      <Name name={props.name} />
-      <Description description={props.description} />
-      <div className={styles.cardFooter}>
-        <div className={styles.parent}>
-          <Price price={props.price} className={styles.inlineBlock} />
-          <Amount amount={props.amount} className={styles.inlineBlock} />
-        </div>
-        <Size size={props.size} unit={props.unit} />
+  <Card title={props.name} text={props.description}>
+    <div className={styles.cardFooter}>
+      <div className={styles.parent}>
+        <Price price={props.price} className={styles.inlineBlock} />
+        <Amount amount={props.amount} className={styles.inlineBlock} />
       </div>
+      <Size size={props.size} unit={props.unit} />
+    </div>
   </Card>
 );
 
