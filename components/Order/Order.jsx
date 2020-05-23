f7fcb85832af47c5eac45e4f70c0fda8c3cae958
lukas braun <lukas.braun@mailbox.org>
2020-05-23 20:16:53 +0200

    Implement Component

--- components/Order/Order.jsx
+++ components/Order/Order.jsx
@@ -1,8 +1,15 @@
 import { Card } from '../Card/Card';
-import { styles } from './Order.module.css';
+import { OrderBadge } from './OrderBadge';
+import styles from './Order.module.css';
 
 export const Order = ({ image, name, description,
   deliveryStatus, orderOrDeliveryDate }) => (
     <Card title={name} text={description} image={image}>
+      <div className={styles.parent}>
+        {deliveryStatus === 'delivered'
+        ? "Erhalten am " + orderOrDeliveryDate
+        : "Bestellt am " + orderOrDeliveryDate}
+        <OrderBadge status={deliveryStatus} />
+      </div>
     </Card>
   );
