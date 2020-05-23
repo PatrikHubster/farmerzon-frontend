3123afa828fc3678c7786fed1316a0fb46cc39ea
lukas braun <lukas.braun@mailbox.org>
2020-05-23 13:19:46 +0200

    Use *.css file

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -8,11 +8,11 @@ import { Card as BCard, Button, Badge } from 'react-bootstrap';
  * children -> will be rendered under description
  */
 const Card = (props) => (
-  <BCard style={{ width: '18rem', margin: '3rem' }} >
+  <BCard className={styles.card} >
     <BCard.Img variant="top" src={props.image ? props.image : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Organic_home-grown_tomatoes_-_unripe_to_ripe.jpg/1024px-Organic_home-grown_tomatoes_-_unripe_to_ripe.jpg"} />
     <BCard.Body>
       <BCard.Title>{props.title}</BCard.Title>
-      <BCard.Text>
+      <BCard.Text className={styles.cardText}>
         {props.text}
       </BCard.Text>
       {props.children ? props.children : ''}
