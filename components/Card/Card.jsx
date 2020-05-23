55ceab863d52919fcf5c7b7e85ac0e02fb915ee5
lukas braun <lukas.braun@mailbox.org>
2020-05-23 12:20:07 +0200

    Refactor Card
    Use Bootstrap Card instead of custom component

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,11 +1,23 @@
 import styles from './Card.module.css';
+import { Card as BCard, Button, Badge } from 'react-bootstrap';
 
+/**
+ * image -> sets Image
+ * title -> e.g. product-name
+ * text  -> e.g. description
+ * children -> will be rendered under description
+ */
 const Card = (props) => (
-  <div className="card">
-    <div className={styles.container}>
-      {props.children}
-    </div>
-  </div>
-)
+  <BCard style={{ width: '18rem', margin: '3rem' }} >
+    <BCard.Img variant="top" src={props.image ? props.image : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Organic_home-grown_tomatoes_-_unripe_to_ripe.jpg/1024px-Organic_home-grown_tomatoes_-_unripe_to_ripe.jpg"} />
+    <BCard.Body>
+      <BCard.Title>{props.title}</BCard.Title>
+      <BCard.Text>
+        {props.text}
+      </BCard.Text>
+      {props.children ? props.children : ''}
+    </BCard.Body>
+  </BCard>
+);
 
 export default Card;
