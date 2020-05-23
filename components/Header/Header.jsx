1d3d678df5b6db62326de6f9b7e7e7fbf6e74706
lukas braun <lukas.braun@mailbox.org>
2020-05-23 11:11:37 +0200

    Finish Header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,12 +1,20 @@
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 export const Header = (props) => (
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+  <Navbar bg="dark" sticky="top" variant="dark">
+    <Navbar.Brand href="/">Farmerzon</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="/profile/articles">Meine Produkte</Nav.Link>
+      <Nav.Link href="/profile/orders">Meine Bestellungen</Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link href="/profile/settings">Einstellungen</Nav.Link>
+      <Nav.Link href="/profile/settings">
+        Profil
+        </Nav.Link>
+      <Nav.Link href="/">
+        Log Out
+    </Nav.Link>
     </Nav>
   </Navbar>
 );
