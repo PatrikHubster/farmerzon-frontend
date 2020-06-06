80bc8d5dbe12159c2cc92539eb92a31c4528f5d4
lukas braun <lukas.braun@mailbox.org>
2020-06-06 19:12:09 +0200

    Site is obsolete

--- pages/profile/add-order.jsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from '../../components/Layout/Layout';
-
-const AddOrder = () => (
-    <Layout title="Bestellen">
-      Bestellungen anzeigen
-    </Layout>
-)
-
-export default AddOrder;
