f274e438c43d7b0f31feec83874af7677ac3478a
lukas braun <lukas.braun@mailbox.org>
2020-06-06 19:11:21 +0200

    Add Component
    Component makes site 'add-order' obsolete

--- components/OrderForm/OrderForm.jsx
+++ components/OrderForm/OrderForm.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import axios from 'axios';
-import { Cookies } from 'react-cookie';
-import { handleAuthSSR } from '../../helper/auth/auth';
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import styles from './OrderForm.module.css';
 
-const cookies = new Cookies();
-
-const OrderForm = (props) => {
-    return (
-        <div>
-          Test
-        </div>
-    )
+export const OrderForm = ({ product, handleSubmit, handleClose, show }) => {
+  const [count, setCount] = useState(0);
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>{product.name}</Modal.Header>
+      <Modal.Body className={styles.body}>
+        <Row>
+          {product.description}
+        </Row>
+        <Row>
+          <input type="number" min="0" max={product.amount}
+            value={count} onChange={(event) => setCount(event.target.value)} />
+        </Row>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Abbrechen
+        </Button>
+        <Button variant="primary" onClick={() => handleSubmit({ ...product, count: +count })}>
+          Bestellen
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
 }
 
-export default OrderForm;
