30ab4f2d884c19f592f4379dc6df8dd9046319d3
patrikhuber <patrihub@icloud.com>
2020-06-21 21:40:10 +0200

    Add requests and set up config for graphql

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,65 @@
-import App from 'next/app';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { Cookies } from 'react-cookie';
+import { config } from '../lib/config';
+import ApolloClient from 'apollo-boost';
+import React from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const MyApp = ({Component, pageProps}) => (
-    <Component {...pageProps} />
-)
+const client = new ApolloClient({
+  uri: `${config.GRAPH_SERVER}/graph`,
+});
+
+const MyApp = ({Component, pageProps}) => {
+  console.log(config.GRAPH_SERVER);
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
 
 export default MyApp;
+
+// -------------
+
+
+// set up cookies
+/*const cookies = new Cookies();
+class Index extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: cookies.get('token') || null
+    }
+  }
+
+  onLoginClick = async () => {
+    console.log("Login called");
+    const response = await axios.get(`${authServer}/authentication/login-username`)
+    const token = response.data.token;
+    cookies.set('token', token);
+    this.setState({
+      token: token
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Main page</h2>
+        <br></br>
+        <button onClick={() => this.onLoginClick()}>Get Token</button>
+        <br></br>
+        <p>Token: {this.state.token}</p>
+        <br></br>
+        <Link href="/secret">
+          <a>Secret page</a>
+        </Link>
+      </div >
+    )
+  }
+}
+
+export default Index;*/
